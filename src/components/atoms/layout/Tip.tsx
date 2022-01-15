7e139e77e2b24e2f553413a3f01705b3bffd1206
Paul Makles <paulmakles@gmail.com>
2022-01-15 23:08:41 +0000

    feat: add Tip

---
+++ src/components/atoms/layout/Tip.tsx
@@ -0,0 +1,51 @@
+import { InfoCircle } from "@styled-icons/boxicons-regular";
+import styled, { css } from "styled-components/macro";
+
+interface Props {
+    readonly palette?: "primary" | "success" | "warning" | "error";
+    readonly children: React.ReactChild;
+}
+
+export const TipBase = styled.div<Omit<Props, "children">>`
+    display: flex;
+    padding: 12px;
+    overflow: hidden;
+    align-items: center;
+
+    font-size: 14px;
+    background: var(--primary-header);
+    border-radius: var(--border-radius);
+
+    a {
+        cursor: pointer;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+
+    svg {
+        flex-shrink: 0;
+        margin-inline-end: 10px;
+    }
+
+    ${(props) =>
+        props.palette !== "primary"
+            ? css`
+                  background: var(--${props.palette});
+                  color: var(--${props.palette}-contrast);
+                  border: 2px solid rgba(var(--${props.palette}-rgb), 0.8);
+              `
+            : css`
+                  border: 2px solid var(--secondary-header);
+              `}
+`;
+
+export function Tip(props: Props) {
+    const { children, ...innerProps } = props;
+    return (
+        <TipBase {...innerProps}>
+            <InfoCircle size={20} />
+            <span>{children}</span>
+        </TipBase>
+    );
+}
