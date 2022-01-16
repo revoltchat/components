d2222fcc988e8345efd08571b9621bf274673e57
Paul Makles <paulmakles@gmail.com>
2022-01-16 11:10:03 +0000

    feat: add IconButton

---
+++ src/components/atoms/inputs/IconButton.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Globe } from "@styled-icons/boxicons-regular";
+
+import { IconButton } from "./IconButton";
+
+export default {
+    title: "Design System/Atoms/Inputs/IconButton",
+    component: IconButton,
+    argTypes: {
+        shape: {
+            name: "Shape",
+            control: "radio",
+            options: ["default", "circle"],
+            defaultValue: "default",
+        },
+    },
+} as ComponentMeta<typeof IconButton>;
+
+const Template: ComponentStory<typeof IconButton> = (args) => (
+    <IconButton {...args} children={<Globe size={32} />} />
+);
+
+export const Normal = Template.bind({});
+
+export const Circle = Template.bind({});
+
+Circle.args = {
+    shape: "circle",
+};
+
+export const Rotated = Template.bind({});
+
+Rotated.args = {
+    rotate: "90deg",
+};
---
+++ src/components/atoms/inputs/IconButton.tsx
@@ -0,0 +1,61 @@
+import styled, { css } from "styled-components/macro";
+
+export interface Props {
+    rotate?: string;
+    shape?: "default" | "circle";
+}
+
+const normal = `var(--secondary-foreground)`;
+const hover = `var(--foreground)`;
+// TODO: add active modifier
+
+export const IconButton = styled.div<Props>`
+    width: fit-content;
+
+    z-index: 1;
+    display: grid;
+    cursor: pointer;
+    place-items: center;
+
+    transition: 0.1s ease all;
+
+    svg {
+        transition: 0.2s ease transform;
+    }
+
+    fill: ${normal};
+    color: ${normal};
+
+    a {
+        color: ${normal};
+    }
+
+    &:hover {
+        fill: ${hover};
+        color: ${hover};
+
+        a {
+            color: ${hover};
+        }
+    }
+
+    ${(props) =>
+        props.shape === "circle" &&
+        css`
+            padding: 4px;
+            border-radius: var(--border-radius-half);
+            background-color: var(--secondary-header);
+
+            &:hover {
+                background-color: var(--primary-header);
+            }
+        `}
+
+    ${(props) =>
+        props.rotate &&
+        css`
+            svg {
+                transform: rotateZ(${props.rotate});
+            }
+        `}
+`;
