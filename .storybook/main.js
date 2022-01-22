7ec3938f7e7e2dec4508bc202dd2a31b0fed295b
trashtemp <96388163+trashtemp@users.noreply.github.com>
2022-01-22 13:16:12 +0100

    fix: added rtl addon to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "storybook-dark-mode",
+        "storybook-rtl-addon",
     ],
     framework: "@storybook/react",
 };
