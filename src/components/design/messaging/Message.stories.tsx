84c65ba6722354d7d2639422af998827cd87705f
Paul Makles <paulmakles@gmail.com>
2022-06-18 15:32:07 +0100

    fix: import changed in file

--- src/components/design/messaging/Message.stories.tsx
+++ src/components/design/messaging/Message.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import Message from "./Message";
+import { Message } from "./Message";
 import {
     ContextDecorator,
     InjectMockClient,
